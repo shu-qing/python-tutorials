62e2f2b8f7239f431f019d9555e5975ffb09f7a3
shuqing <shuqing@aibayes.com>
2020-01-13 22:49:02 +0800

    add 004_while.py

---
+++ basic/004_while.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# filename：for.py
+# author: shuqing
+
+i = 0
+while i < 10:
+    print(i)
+    i += 2
+
+j = 0
+while j in range(1, 9):
+    print(j)
+    j += 1
