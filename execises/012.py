5031e3790c1380fcfddfdc48c1782a3a0baed85a
shuqing <shuqing@aibayes.com>
2020-01-23 09:20:28 +0800

    add 012.py

---
+++ execises/012.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# filename：011.py
+# author: shuqing
+
+# 题目：判断101-200之间有多少个素数，并输出所有素数。
+
+num = 0
+for i in range(101, 201):
+    for j in range(2, i):
+        if(i % j == 0):
+            break
+        if (j == i-1):
+            print(i)
+            num += 1
+print(num)
